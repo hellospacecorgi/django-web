0b8a4e3f6c2246992e2b1147d16144fe54299318
Flora Chan <hcha8985@uni.sydney.edu.au>
2021-09-10 21:34:13 +1000

    Looking into pages/views.py functions input request/*args/**kwargs

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Handling web pages views as functions
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
 	# *args and **kwards are python specific - look it up
+	print(*args, **kwargs)
+	print(request.user)
 	return HttpResponse("<h1>Hello Django!</h1>")
 
-def contact_view(*args, **kwargs):
+def contact_view(request, *args, **kwargs):
 	return HttpResponse("<h1>Contact Page</h1>")
