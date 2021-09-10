41870c72b0c6874ce357df98fca9e245361e8314
Flora Chan <hcha8985@uni.sydney.edu.au>
2021-09-10 21:12:27 +1000

    Customised field types in product

--- src/products/models.py
+++ src/products/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 # Product to inherit the Model class
 class Product(models.Model):
-	title		= models.TextField()
-	description	= models.TextField()
-	price 		= models.TextField()
+
+# use different model field types to customised usage
+	title		= models.CharField(max_length=100)
+	description	= models.TextField(blank=True, null=True)
+	price 		= models.DecimalField(max_digits=10,decimal_places=2)
 	summary		= models.TextField(default='this is a default summary')
 
 """
