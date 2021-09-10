56707beb8a0c4c1206b20f6bea2b3ffbc5f59dff
Flora Chan <hcha8985@uni.sydney.edu.au>
2021-09-10 21:19:46 +1000

    Featured boolean field

--- src/products/models.py
+++ src/products/models.py
@@ -5,10 +5,12 @@ class Product(models.Model):
 
 # use different model field types to customised usage
 	title		= models.CharField(max_length=100)
+	# blank is how the field is rendered (required/bold or not)
+	# null is about the database (empty in database)
 	description	= models.TextField(blank=True, null=True)
 	price 		= models.DecimalField(max_digits=10,decimal_places=2)
 	summary		= models.TextField(default='this is a default summary')
-
+	featured	= models.BooleanField(default=False)
 """
 After changes to models.py , in terminal run
 python manage.py makemigrations
