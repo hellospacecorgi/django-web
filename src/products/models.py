acf8ec0e01a10ff32fbcdeba9aacb50187ec80db
Flora Chan <hcha8985@uni.sydney.edu.au>
2021-09-10 20:58:48 +1000

    Textfields in product

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Product to inherit the Model class
+class Product(models.Model):
+	title		= models.TextField()
+	description	= models.TextField()
+	price 		= models.TextField()
+	summary		= models.TextField(default='this is a default summary')
+
+"""
+After changes to models.py , in terminal run
+python manage.py makemigrations
+python manage.py migrate
+"""
